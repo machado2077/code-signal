6e22d7752026864f490a42d901c21a2b0ae6320e
matheus <machadom@outlook.com.br>
2020-04-12 20:02:35 -0300

    criado e iniciado o exercicio reverseInParentheses

---
+++ reverseInParentheses.py
@@ -0,0 +1,15 @@
+def reverseInParentheses(inputString):
+    #varrer a string e mapear os () 
+    #inserir os "(" em um coordenadas bem como os ")"
+    #unir os () extremos 
+    map_open = []  
+    map_close = []
+    for i in range(0, len(inputString)):
+        if inputString[i] == '(':
+            map_open.append(i)
+        elif inputString[i] == ')':
+            map_close.append(i)
+    #inverter as strings dos () internos para os externos
+    #(bar(baz))  = [0, 4], [8, 9]   => [4, 0], [8, 9] resolve
+    
+    #(bar)baz(blim) = [0, 8], [4, 13]
