fab0b0df23d18831e1068914606d356d44e2a7c0
matheus <machadom@outlook.com.br>
2020-04-13 19:55:32 -0300

    finalizado o exercicio reverseInParentheses V3

--- reverseInParentheses.py
+++ reverseInParentheses.py
@@ -1,22 +1,32 @@
-def reverseInParentheses(inputString):
-    # BRANCH: reverseV3
-    #varrer a string e mapear os () 
-    #inserir os "(" em um coordenadas bem como os ")"
-    #unir os () extremos 
-    map_open = []  
-    map_close = []
-    for i in range(0, len(inputString)):
-        if inputString[i] == '(':
-            map_open.append(i)
-        elif inputString[i] == ')':
-            map_close.append(i)
-    #inverter as strings dos () internos para os externos
-    #(bar(baz))  = [0, 4], [8, 9]   => [4, 0], [8, 9] resolve
-    
-    #(bar)baz(blim) = [0, 8], [4, 13]
-
+# BRANCH: reverseV3    
     #TENTAR UMA VARREDURA DO TIPO:
-        '''
-        se o próximo bracket for == (, indice = i
-        se o próximo bracket for == ), a palavra partindo de indice + i até i será invertida
-        '''
+''' 
+    - inicio = fim = 0
+    - enquanto houver brackets, a varredura continua
+        - se o próximo bracket for == (, inicio = i
+        - se o próximo bracket for == ), fim = i
+             VARREDURAS SUCESSIVAS
+            -a palavra partindo do inicio ao fim será ivertida
+            - remover os brackets referentes aos indices (inicio e fim)
+            - break no laço interno
+'''
+
+def reverseInParentheses(inputString):    
+    listaStr = list(inputString)
+    inputString = ''
+    inicio = fim = 0
+    while True:
+        if not ('(' and ')') in listaStr:
+            break
+        for i in range(len(listaStr)):
+            if listaStr[i] == '(':
+                inicio = i
+            elif listaStr[i] == ')':
+                fim = i
+                listaStr[inicio+1:fim] = listaStr[fim-1:inicio:-1]
+                del listaStr[inicio]
+                del listaStr[fim-1]
+                break
+    for element in listaStr:
+            inputString += element
+    return inputString
