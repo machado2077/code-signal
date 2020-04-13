04a82cdcd182b1d83dfbda4f043159f68aaec154
matheus <machadom@outlook.com.br>
2020-04-12 22:53:27 -0300

    estruturado o padrão no reverseV2

--- reverseInParentheses.py
+++ reverseInParentheses.py
@@ -11,6 +11,11 @@ def reverseInParentheses(inputString):
         elif inputString[i] == ')':
             map_close.append(i)
     #inverter as strings dos () internos para os externos
-    #(bar(baz))  = [0, 4], [8, 9]   => [4, 0], [8, 9] resolve
+    #(bar(baz))  ; a = [0, 4], b = [8, 9]   => [4, 0], [8, 9] resolve => (4, 8),                                                                    (0, 9)
+        # nesse caso, a[1] < b[0]
+    #(bar)baz(blim) ; a = [0, 8], b = [4, 13] => (0, 4), (8, 13)
+        #nesse caso, a[1] > a[0]
+
+    # (bar(kor)(zir)) ; a = [0, 4, 8], b = [7, 12, 13] => (0, 13), (4, 7), (8, 12)
+        #nesse caso, a[1] < b[0]
     
-    #(bar)baz(blim) = [0, 8], [4, 13]
