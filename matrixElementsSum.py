6faa5f71bc789a796c102b9d22acbe9a7ce0308c
matheus <machadom@outlook.com.br>
2020-04-14 22:42:22 -0300

    determinado função e teste no mesmo arquivo em matrixElementsSum.py

--- matrixElementsSum.py
+++ matrixElementsSum.py
@@ -17,3 +17,23 @@ def matrixElementsSum(matrix):
             if validade == True:                
                 soma += matrix[linha][col]        
     return soma
+
+#---------------------------------------------------------------------------------
+import unittest
+
+class matrixElementsSumTest(unittest.TestCase):
+    
+    samples = [
+    [[0,1,1,2], [0,5,0,0], [2,0,3,3]],
+    [[1,1,1,0], [0,5,0,1], [2,1,3,10]]
+    ]
+
+    def test_1(self):                    
+        self.assertEqual(matrixElementsSum(self.samples[0]), 9)
+
+    def test_2(self):
+        soma = matrixElementsSum(self.samples[1])
+        self.assertEqual(soma, 9)
+
+if __name__ == '__main__':
+    unittest.main()
